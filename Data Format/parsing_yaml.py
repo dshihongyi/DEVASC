9ed7c198e98c60ca03a5e7679ac2fd2cabe72c79
dshihongyi <shihongyivc@hotmail.com>
2021-01-21 14:07:42 +1300

    Parsing Yaml

--- Data Format/parsing_yaml.py
+++ Data Format/parsing_yaml.py
@@ -7,4 +7,8 @@ with open("address_list.yaml") as f:
 with open("test.yaml", "w") as f:
     yaml.dump(yaml_dic, f)
 
+
+print(type(yaml_dic['addresses'][2]))
+print(type(yaml_dic['addresses']))
 print(yaml_dic)
+print(yaml_dic['addresses'][1])
