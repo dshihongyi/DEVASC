c5f06fee54c0f60691bebb845cf79225d7641dae
dshihongyi <shihongyivc@hotmail.com>
2021-02-04 23:56:19 +1300

    Template organized

--- NETCONF/NETCONF_EDIT/test.py
+++ NETCONF/NETCONF_EDIT/test.py
@@ -4,22 +4,27 @@ import xml.dom.minidom
 import sys
 import json
 
-def options():
-    print("-" * 65)
-    option = int(input("Choose your Option: ")) + 500
-    print("-" * 80)
-        # if option == 501:
-        # print("\n" + "-" * 80)
-    id = int(input("Input Loopback Number: "))
-    return [option, id]
+# def options():
+#     print("-" * 65)
+#     option = int(input("Choose your Option: ")) + 500
+#     print("-" * 80)
+#         # if option == 501:
+#         # print("\n" + "-" * 80)
+#     id = int(input("Input Loopback Number: "))
+#     return [option, id]
 
 
-def main():
-    number = options()
-    str_number = [str(int) for int in number]
-    print(str_number)
-    print(type(str_number[0]))
-    print(str_number[1])
+# def main():
+#     number = options()
+#     str_number = [str(int) for int in number]
+#     print(str_number)
+#     print(type(str_number[0]))
+#     print(str_number[1])
 
-if __name__ == '__main__':
-    sys.exit(main())
+# if __name__ == '__main__':
+#     sys.exit(main())
+
+
+id = int(input("Remove Loopback Number: "))
+
+print(id)
--- NETCONF/NETCONF_GET_EDIT/netconf_get_edit.py
+++ NETCONF/NETCONF_GET_EDIT/netconf_get_edit.py
@@ -9,7 +9,6 @@ router = {"host": "ios-xe-mgmt.cisco.com", "port": "10000",
 
 # loopback = {"id": "98", "desc":"mytest", "ip": "10.98.98.1", "prefix": "255.255.255.0"}
 
-
 # Function of Get request
 def get_request(xmlstring):
     print("XML FILTER")
@@ -45,6 +44,7 @@ def edit_request(xmlstring):
 
 def options ():
     getlist = [101,102,103,104,201,202,203,204,301,302,303,304,501,502,503,504]
+    print("\nWELCOME TO THE CONFIGURE PAGE")
     print("\n" + "-" * 90)
     print("""| Fecthing Data From CSR1000                                                             |
 |   > 0.Show Running   > 1.Show Interface  > 2. Show Routing  > 3. Show System           |""")
@@ -61,7 +61,7 @@ def options ():
         print("-" * 80)
         print("NETCONF GET - RUNNING CONFIGURE")
         print("-" * 80)
-        return [option]
+        return [option,0,0,0,0]
 
     elif option == "1":
         print("-" * 80)
@@ -76,7 +76,7 @@ def options ():
         option = int(input("Choose your Option: ")) + 100
         print("-" * 80)
         if option in getlist:
-            return [option]
+            return [option,0,0,0,0]
 
     elif option == "2":
         print("-" * 80)
@@ -89,8 +89,8 @@ def options ():
         option = int(input("Choose your Option: ")) + 200
         print("-" * 80)
         if option in getlist:
-            return [option]
-    
+            return [option,0,0,0,0]
+
     elif option == "3":
         print("-" * 80)
         print("\nNETCONF GET - SYSTEM CONFIGURE")
@@ -102,16 +102,15 @@ def options ():
         option = int(input("Choose your Option: ")) + 300
         print("-" * 80)
         if option in getlist:
-            return [option]
+            return [option,0,0,0,0]
         
     elif option == "5":
-
         print("-" * 80)
         print("\nNETCONF EDIT - INTERFACE CONFIGURE")
         print("-" * 65)
         print("""| MODIFYING Interface Configure                                 |
-|   1. Create New Loopback        2. Modify Exist Loopback      |  
-|   3. Remove Exist Loopback      4. Modify Gigabit Interface   |""")
+|   1. Create New Loopback        3. Modify Exist Loopback      |  
+|   2. Remove Exist Loopback      4. Modify Gigabit Interface   |""")
         print("-" * 65)
         option = int(input("Choose your Option: ")) + 500
         print("-" * 80)
@@ -121,12 +120,52 @@ def options ():
             desc = input("Input Loopback Description: ")
             ip = input("Input Loopback IP Address: ")
             prefix = input("Input Loopback Subnet Mask: ")
-            return [option, id, desc, ip, prefix]
-        # if option in getlist:
-        #     return option
-    
+            if option in getlist:
+                return [option, id, desc, ip, prefix]
+        
+        if option == 501:
+            print("\n" + "-" * 80)
+            id = int(input("Input Loopback Number: "))
+            desc = input("Input Loopback Description: ")
+            ip = input("Input Loopback IP Address: ")
+            prefix = input("Input Loopback Subnet Mask: ")
+            if option in getlist:
+                return [option, id, desc, ip, prefix] 
+
+        if option == 502:
+            print("\n" + "-" * 80)
+            id = int(input("Remove Loopback Number: ") or "0")
+            if id == 0:
+                options()
+            elif option in getlist:
+                return [option, id, 0,0,0]
+
+        if option == 503:
+            print("\n" + "-" * 80)
+            id = int(input("Modify Loopback Number: ") or "0")
+            desc = input("New Description: ")
+            ip = input("New IP Address: ")
+            prefix = input("New Subnet Mask: ")
+            if id == 0:
+                options()
+            elif option in getlist:
+                return [option, id, desc, ip, prefix]
+
+        if option == 504:
+            print("\n" + "-" * 80)
+            id = int(input("Modify Gigabit Number: ") or "0")
+            desc = input("New Description: ")
+            # ip = input("New IP Address: ")
+            # prefix = input("New Subnet Mask: ")
+            if id == 0:
+                options()
+            elif option in getlist:
+                return [option, id, desc, 0,0]
+            
     else:
         print("INPUT WRONG VALUE!!! LET'S CHOOSE AGAIN\n")
+    options()
+
 
 number = options()
 str_number = [str(int) for int in number]
@@ -135,17 +174,46 @@ desc = str_number[2]
 ip = str_number[3]
 prefix = str_number[4]
 
+# String Option - Fetch Data - Filter Template
+xml_filter_running = open("e:/Github/DEVASC/NETCONF/NETCONF_GET_EDIT/Filter_Template/netconf_get_0.xml").read()
+xml_filter_inter   = open("e:/Github/DEVASC/NETCONF/NETCONF_GET_EDIT/Filter_Template/netconf_get_101.xml").read()
+xml_filter_gbinter = open("e:/Github/DEVASC/NETCONF/NETCONF_GET_EDIT/Filter_Template/netconf_get_102.xml").read()
+xml_filter_lpinter = open("e:/Github/DEVASC/NETCONF/NETCONF_GET_EDIT/Filter_Template/netconf_get_103.xml").read()
+xml_filter_ipinter = open("e:/Github/DEVASC/NETCONF/NETCONF_GET_EDIT/Filter_Template/netconf_get_104.xml").read()
+xml_filter_router  = open("e:/Github/DEVASC/NETCONF/NETCONF_GET_EDIT/Filter_Template/netconf_get_201.xml").read()
+xml_filter_host    = open("e:/Github/DEVASC/NETCONF/NETCONF_GET_EDIT/Filter_Template/netconf_get_301.xml").read()
+xml_filter_user    = open("e:/Github/DEVASC/NETCONF/NETCONF_GET_EDIT/Filter_Template/netconf_get_302.xml").read()
+xml_filter_lic     = open("e:/Github/DEVASC/NETCONF/NETCONF_GET_EDIT/Filter_Template/netconf_get_303.xml").read()
+xml_filter_mgmt    = open("e:/Github/DEVASC/NETCONF/NETCONF_GET_EDIT/Filter_Template/netconf_get_304.xml").read()
+
+# String Option 501 - Create Loopback Interface
+netconf_edit_501 = open("e:/Github/DEVASC/NETCONF/NETCONF_GET_EDIT/Filter_Template/netconf_edit_501.xml").read()
+xml_filter_post_lpinter = netconf_edit_501.format(ide = ide, desc = desc, ip = ip, prefix = prefix)
+
+# String Option 502 - Delete Loopback Interface
+netconf_edit_502 = open("e:/Github/DEVASC/NETCONF/NETCONF_GET_EDIT/Filter_Template/netconf_delete_502.xml").read()
+xml_filter_del_lpinter = netconf_edit_502.format(ide = ide)
+
+# String Option 503 - Edit Loopback Interface
+netconf_edit_503 = open("e:/Github/DEVASC/NETCONF/NETCONF_GET_EDIT/Filter_Template/netconf_edit_503.xml").read()
+xml_filter_put_lpinter = netconf_edit_503.format(ide = ide, desc = desc, ip = ip, prefix = prefix)
+
+# String Option 504 - Edit Gigabit Interface
+netconf_edit_504 = open("e:/Github/DEVASC/NETCONF/NETCONF_GET_EDIT/Filter_Template/netconf_edit_504.xml").read()
+xml_filter_put_gpinter = netconf_edit_504.format(ide = ide, desc = desc)
+
+
 def result():
-    if   str_number == "0":
+    if   str_number[0] == "0":
         get_request(xml_filter_running)
     elif str_number[0] == "101":
-        get_request(xml_filter_interface)
+        get_request(xml_filter_inter)
     elif str_number[0] == "102":
-        get_request(xml_filter_gbinterface)
+        get_request(xml_filter_gbinter)
     elif str_number[0] == "103":
-        get_request(xml_filter_lpinterface)
+        get_request(xml_filter_lpinter)
     elif str_number[0] == "104":
-        get_request(xml_filter_ipinterface)
+        get_request(xml_filter_ipinter)
     elif str_number[0] == "201":
         get_request(xml_filter_router)
     elif str_number[0] == "301":
@@ -157,148 +225,17 @@ def result():
     elif str_number[0] == "304":
         get_request(xml_filter_mgmt)
     elif str_number[0] == "501":
-        edit_request(xml_filter_post_lpinterface)
+        edit_request(xml_filter_post_lpinter)
+    elif str_number[0] == "502":
+        edit_request(xml_filter_del_lpinter)
+    elif str_number[0] == "503":
+        edit_request(xml_filter_put_lpinter)
     else:
         print("\nInvalid Option - Do Again!!!")
         main()
 
 
-
-
-# String Option 0
-xml_filter_running = """
-<filter>
-  <native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
-  </native>
-</filter>
-"""
-# String Option 101
-xml_filter_interface = """
-<filter>
-  <native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
-  <interface>
-  </interface>
-  </native>
-</filter>
-"""
-
-# String Option 102
-xml_filter_gbinterface = """
-<filter>
-  <native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
-    <interface>
-        <GigabitEthernet> 
-        </GigabitEthernet> 
-    </interface>
-  </native>
-</filter>
-"""
-
-# String Option 103
-xml_filter_lpinterface = """
-<filter>
-  <native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
-    <interface>
-        <Loopback>
-        </Loopback>
-    </interface>
-  </native>
-</filter>
-"""
-
-# String Option 104
-xml_filter_ipinterface = """
-<filter>
-  <native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
-    <interface>
-        <GigabitEthernet>
-            <ip>
-                <address>
-                </address>
-            </ip>
-        </GigabitEthernet> 
-    </interface>
-  </native>
-</filter>
-"""
-# String Option 201
-xml_filter_router = """
-<filter>
-  <native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
-  <router>
-  </router>
-  </native>
-</filter>
-"""
-
-# String Option 301
-xml_filter_host = """
-<filter>
-  <native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
-  <hostname>
-  </hostname>
-  </native>
-</filter>
-"""
-
-# String Option 302
-xml_filter_user = """
-<filter>
-  <native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
-  <username>
-  </username>
-  </native>
-</filter>
-"""
-
-# String Option 303
-xml_filter_lic = """
-<filter>
-  <native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
-  <license>
-  </license>
-  </native>
-</filter>
-"""
-
-# String Option 304
-xml_filter_mgmt = """
-<filter>
-  <native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
-  <line>
-  </line>
-  </native>
-</filter>
-"""
-
-# String Option 501
-xml_filter_post_lpinterface = """
-<config>
-  <native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
-    <interface>
-        <Loopback>
-            <name>{ide}</name>
-            <description>{desc}</description>
-            <ip>
-                <address>
-                    <primary>
-                        <address>{ip}</address>
-                        <mask>{prefix}</mask>
-                    </primary>
-                </address>
-            </ip>
-        </Loopback>
-    </interface>
-  </native>
-</config>
-""".format(ide = ide, desc = desc, ip = ip, prefix = prefix)
-
-
-
 def main():
-    # a = options()
-    # print (a[0])
-    # print(type(number[0])
     result()
 
 if __name__ == '__main__':
