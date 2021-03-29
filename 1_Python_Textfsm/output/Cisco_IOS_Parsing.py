eb832dc8fd0b6b7621b8a2451ac978fc0a44b7b6
dshihongyi <shihongyivc@hotmail.com>
2021-03-29 23:59:29 +1300

    textfsm

---
+++ 1_Python_Textfsm/output/Cisco_IOS_Parsing.py
@@ -0,0 +1,18 @@
+import sys
+import textfsm
+from tabulate import tabulate
+
+template = sys.argv[1]
+output_file = sys.argv[2]
+
+with open(template) as f, open(output_file) as output:
+    re_table = textfsm.TextFSM(f)
+    header = re_table.header
+    result = re_table.ParseText(output.read())
+    # print(result)
+    print(tabulate(result, headers=header))
+
+## Print the result ##
+##  $ python output/Cisco_IOS_Parsing.py output/cisco_ios_show_interfaces.textfsm output/show_int  ##
+##  $ python output/Cisco_IOS_Parsing.py output/cisco_ios_show_interfaces.textfsm output/show_int > parse.csv ##
+## https://pyneng.readthedocs.io/en/latest/book/21_textfsm/textfsm_examples.html ##
