3f82d930b180bd2b2e7c93209e02cc78ec018659
dshihongyi <shihongyivc@hotmail.com>
2021-05-15 23:39:54 +1200

    textfsm

--- 1_Python_Textfsm/Cisco_IOS_Parsing.py
+++ 1_Python_Textfsm/Cisco_IOS_Parsing.py
@@ -3,7 +3,7 @@ import textfsm
 from tabulate import tabulate
 
 template = open("E:/Github/DEVASC/1_Python_Textfsm/output/cisco_ios_show_interfaces.template")
-output_file = open("E:/Github/DEVASC/1_Python_Textfsm/output/show_int")
+output_file = open("E:/Github/DEVASC/1_Python_Textfsm/output/show_int1")
 
 with template as f, output_file as output:
     re_table = textfsm.TextFSM(f)
